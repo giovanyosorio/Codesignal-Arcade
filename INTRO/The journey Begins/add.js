3c5c528efbe289281568478b4007cdd457cee644
giovany osorio <giovany.torres.799@gmail.com>
2023-07-24 18:59:46 -0500

    intro arcade code-signal

---
+++ INTRO/The journey Begins/add.js
@@ -0,0 +1,34 @@
+/* Write a function that returns the sum of two numbers.
+
+Example
+
+For param1 = 1 and param2 = 2, the output should be
+solution(param1, param2) = 3.
+
+Input/Output
+
+[execution time limit] 4 seconds (js)
+
+[memory limit] 1 GB
+
+[input] integer param1
+
+Guaranteed constraints:
+-1000 ≤ param1 ≤ 1000.
+
+[input] integer param2
+
+Guaranteed constraints:
+-1000 ≤ param2 ≤ 1000.
+
+[output] integer
+
+The sum of the two inputs.
+
+[JavaScript] Syntax Tips */
+
+function solution(param1, param2) {
+    return param1+param2
+    }
+    
+    solution(1,2)
