239a952af35775857829b8f9d7c297e5dcea3a23
giovany osorio <giovany.torres.799@gmail.com>
2023-07-24 19:06:25 -0500

    check-palindrome

---
+++ INTRO/The journey Begins/checkPalindrome.js
@@ -0,0 +1,29 @@
+/* For inputString = "aabaa", the output should be
+solution(inputString) = true;
+For inputString = "abac", the output should be
+solution(inputString) = false;
+For inputString = "a", the output should be
+solution(inputString) = true.
+Input/Output
+
+[execution time limit] 4 seconds (js)
+
+[memory limit] 1 GB
+
+[input] string inputString
+
+A non-empty string consisting of lowercase characters.
+
+Guaranteed constraints:
+1 ≤ inputString.length ≤ 105.
+
+[output] boolean
+
+true if inputString is a palindrome, false otherwise. */
+
+function solution(inputString) {
+    return inputString == inputString.split('').reverse().join('')
+    
+    
+    }
+    
